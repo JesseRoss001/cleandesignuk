c98f38c190d379a00af39f609ef7e3378fea416d
Jesse Lee Ross <jesseross001@gmail.com>
2024-05-23 12:00:34 +0000

    update git push origin main

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -1,6 +1,10 @@
 import os
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+
+# Define BASE_DIR
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
