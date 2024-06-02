919ec9db4cad35b76cc008372d5359ec4997f407
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-06-02 23:44:12 +0000

    Add custom error pages for 404, 403, and 400

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import handler500
+from django.conf.urls import handler500 , handler404, handler403, handler400
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 handler500 = views.handler500
-
+handler404 = views.handler404
+handler403 = views.handler403
+handler400 = views.handler400
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -4,3 +4,18 @@ def handler500(request, *args, **argv):
     response = render(request, 'errors/500.html', {})
     response.status_code = 500
     return response
+
+def handler404(request, exception, template_name="errors/404.html"):
+    response = render(request, template_name)
+    response.status_code = 404
+    return response
+
+def handler403(request, exception, template_name="errors/403.html"):
+    response = render(request, template_name)
+    response.status_code = 403
+    return response
+
+def handler400(request, exception, template_name="errors/400.html"):
+    response = render(request, template_name)
+    response.status_code = 400
+    return response
