d3c7bbded88b4e7828f1bab22fdc5a0c93fee742
Jesse Lee Ross <jesseross001@gmail.com>
2024-06-04 12:05:59 +0000

    update part of booking

--- myproject/bookings/admin.py
+++ myproject/bookings/admin.py
@@ -4,6 +4,5 @@ from .models import Booking
 class BookingAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'date', 'time_slot', 'is_available')
     list_filter = ('date', 'is_available')
-    search_fields = ('full_name', 'email', 'phone_number')
 
 admin.site.register(Booking, BookingAdmin)
