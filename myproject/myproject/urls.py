22359aaface3124cabba3f0b8aa8670556ec0bc1
Jesse Lee Ross <jesseross001@gmail.com>
2024-06-20 12:27:18 +0000

    add sitemap

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,15 +1,16 @@
-from django.contrib.sitemaps.views import sitemap
+
 from django.contrib import admin
 from django.urls import include, path
-from blog.sitemaps import BlogPostSitemap
 from django.conf.urls import handler500 , handler404, handler403, handler400
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 from . import views
 
-sitemaps = {
-    'posts': BlogPostSitemap,
-}
+
+
+
+
 
 handler500 = views.handler500
 handler404 = views.handler404
@@ -24,8 +25,13 @@ urlpatterns = [
     path('services/', include('packages_services.urls', namespace='services')),
     path('contact/', include('contact.urls', namespace='contact')),
     path('bookings/', include('bookings.urls', namespace='bookings')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('store/', include('developer_store.urls', namespace='developer_store')),
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path('sitemap.xml', serve, {
+        'path': 'sitemap.xml',
+        'document_root': settings.STATIC_ROOT,
+    }),
+]
