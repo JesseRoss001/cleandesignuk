0f9d8bf76a3633481c00c7870570ebee8765d1d3
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-08-31 15:55:58 +0100

    add facebook page

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, facebook_page
 
 app_name = 'home'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('facebook-page/', facebook_page, name='facebook_page'),  # New URL pattern for the Facebook page
 ]
--- myproject/home/views.py
+++ myproject/home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+def facebook_page(request):
+    return render(request, 'home/facebook_page.html')
